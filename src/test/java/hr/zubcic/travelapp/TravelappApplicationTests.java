5da9aeafc62e0a7de015d0d218ce24307c26b2b3
Lovro Zubcic <lovro.zubcic@gmail.com>
2021-05-15 17:56:50 +0200

    Initial commit

---
+++ src/test/java/hr/zubcic/travelapp/TravelappApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.zubcic.travelapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
