dc2ae60b7831ffc3da60e71aef8e4858345b4bcb
Lovro Zubčić <lovro.zubcic@gmail.com>
2021-05-20 22:07:51 +0000

    Add update and delete

--- src/main/java/hr/zubcic/travelapp/services/TravelServiceImpl.java
+++ src/main/java/hr/zubcic/travelapp/services/TravelServiceImpl.java
@@ -1,11 +1,14 @@
 package hr.zubcic.travelapp.services;
 
 import hr.zubcic.travelapp.dto.TravelDTO;
+import hr.zubcic.travelapp.dto.command.TravelCommand;
 import hr.zubcic.travelapp.model.Travel;
 import hr.zubcic.travelapp.repositories.TravelRepository;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -23,8 +26,28 @@ public class TravelServiceImpl implements TravelService {
     }
 
     @Override
-    public TravelDTO findByTravelName(String travelName) {
-        return travelRepository.findByTravelName(travelName).map(this::mapTravelToDTO).orElse(null);
+    public TravelDTO findByTravelId(Long id) {
+        return travelRepository.findById(id).map(this::mapTravelToDTO).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public Optional<TravelDTO> update(Long id, TravelCommand updateTravel) {
+        Travel travelToUpdate = travelRepository.findById(id).orElse(null);
+        if (travelToUpdate != null) {
+            travelToUpdate.setTravelName(updateTravel.getTravelName());
+            travelToUpdate.setShortDescription(updateTravel.getShortDescription());
+            travelToUpdate.setPrice(updateTravel.getPrice());
+            travelToUpdate.setSpaceLeft(updateTravel.getSpaceLeft());
+            return Optional.of(mapTravelToDTO(travelToUpdate));
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public void deleteTravel(Long id) {
+        Travel travel = travelRepository.findById(id).orElse(null);
+        travelRepository.delete(travel);
     }
 
     private TravelDTO mapTravelToDTO(final Travel travel) {
